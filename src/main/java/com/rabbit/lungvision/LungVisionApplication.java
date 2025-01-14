46f9660309acc110b5712dcff1a5e41b6c5d8632
TuZaaaaa <2677824148wjt@gmail.com>
2025-01-14 20:46:41 +0800

    chore: init

---
+++ src/main/java/com/rabbit/lungvision/LungVisionApplication.java
@@ -0,0 +1,13 @@
+package com.rabbit.lungvision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LungVisionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LungVisionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rabbit/lungvision/LungVisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabbit.lungvision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LungVisionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
